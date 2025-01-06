d85a4e97edb1340fd25cff82628bc43965eedae3
Betim B <betimbegisholli0@gmail.com>
2025-01-06 21:54:15 +0100

    gjuha

---
+++ assets/js/language.js
@@ -0,0 +1,20 @@
+function changeLanguage(lang) {
+          
+    document.querySelectorAll('.translate').forEach(el => {
+        const translation = el.dataset[`lang${lang.charAt(0).toUpperCase() + lang.slice(1)}`];
+        if (translation) el.textContent = translation;
+    });
+
+    localStorage.setItem('language', lang);
+    
+    document.querySelectorAll('.language-menu').forEach(btn => {
+        const isActive = btn.dataset.lang === lang;
+        btn.classList.toggle('active', isActive);
+    });
+}
+
+// Al as default language
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLanguage = localStorage.getItem('language') || 'al';
+    changeLanguage(savedLanguage);
+});
